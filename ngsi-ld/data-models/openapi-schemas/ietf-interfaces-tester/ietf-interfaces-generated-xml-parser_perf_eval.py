9bf6169fcb743b1d4b194c225ba9bff43681c7fb
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-07-12 09:46:32 +0200

    Update ietf-interfaces-generated-xml-parser_perf_eval.py

--- ngsi-ld/data-models/openapi-schemas/ietf-interfaces-tester/ietf-interfaces-generated-xml-parser_perf_eval.py
+++ ngsi-ld/data-models/openapi-schemas/ietf-interfaces-tester/ietf-interfaces-generated-xml-parser_perf_eval.py
@@ -26,6 +26,27 @@ interface_dict_buffers = []
 # from ngsi_ld_models.models.statistics import Statistics
 interface_statistics_dict_buffers = []
 
+# from ngsi_ld_models.models.ipv4 import Ipv4
+interface_ipv4_dict_buffers = []
+
+# from ngsi_ld_models.models.address import Address
+interface_ipv4_address_dict_buffers = []
+
+# from ngsi_ld_models.models.neighbor import Neighbor
+interface_ipv4_neighbor_dict_buffers = []
+
+# from ngsi_ld_models.models.ipv6 import Ipv6
+interface_ipv6_dict_buffers = []
+
+# from ngsi_ld_models.models.address import Address
+interface_ipv6_address_dict_buffers = []
+
+# from ngsi_ld_models.models.neighbor import Neighbor
+interface_ipv6_neighbor_dict_buffers = []
+
+# from ngsi_ld_models.models.autoconf import Autoconf
+interface_ipv6_autoconf_dict_buffers = []
+
 parsing_exec_times = []
 EXEC_TIMES = 1000
 
@@ -118,7 +139,7 @@ for i in range (0, EXEC_TIMES):
             interface_statistics_dict_buffer["type"] = "Statistics"
             interface_statistics_dict_buffer["isPartOf"] = {}
             interface_statistics_dict_buffer["isPartOf"]["type"] = "Relationship"
-            interface_statistics_dict_buffer["isPartOf"]["object"] = "urn:ngsi-ld:Interface:" + interface_dict_buffer["name"]["value"]
+            interface_statistics_dict_buffer["isPartOf"]["object"] = interface_dict_buffer["id"]
             discontinuityTime = statistics.find(".//{urn:ietf:params:xml:ns:yang:ietf-interfaces}discontinuity-time")
             if discontinuityTime is not None:
                 element_text = discontinuityTime.text
@@ -204,7 +225,213 @@ for i in range (0, EXEC_TIMES):
                 interface_statistics_dict_buffer["outErrors"]["type"] = "Property"
                 interface_statistics_dict_buffer["outErrors"]["value"] = int(element_text)
             interface_statistics_dict_buffers.append(interface_statistics_dict_buffer)
-        interface_dict_buffers.append(interface_dict_buffer)
+        for ipv4 in interface.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ipv4"):
+            interface_ipv4_dict_buffer = {}
+            interface_ipv4_dict_buffer["id"] = "urn:ngsi-ld:Ipv4:" + interface_dict_buffer["id"].split(":")[-1]
+            interface_ipv4_dict_buffer["type"] = "Ipv4"
+            interface_ipv4_dict_buffer["isPartOf"] = {}
+            interface_ipv4_dict_buffer["isPartOf"]["type"] = "Relationship"
+            interface_ipv4_dict_buffer["isPartOf"]["object"] = interface_dict_buffer["id"]
+            enabled = ipv4.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}enabled")
+            if enabled is not None:
+                element_text = enabled.text
+                interface_ipv4_dict_buffer["enabled"] = {}
+                interface_ipv4_dict_buffer["enabled"]["type"] = "Property"
+                interface_ipv4_dict_buffer["enabled"]["value"] = element_text.capitalize()
+            forwarding = ipv4.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}forwarding")
+            if forwarding is not None:
+                element_text = forwarding.text
+                interface_ipv4_dict_buffer["forwarding"] = {}
+                interface_ipv4_dict_buffer["forwarding"]["type"] = "Property"
+                interface_ipv4_dict_buffer["forwarding"]["value"] = element_text.capitalize()
+            mtu = ipv4.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}mtu")
+            if mtu is not None:
+                element_text = mtu.text
+                interface_ipv4_dict_buffer["mtu"] = {}
+                interface_ipv4_dict_buffer["mtu"]["type"] = "Property"
+                interface_ipv4_dict_buffer["mtu"]["value"] = int(element_text)
+            for address in ipv4.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}address"):
+                interface_ipv4_address_dict_buffer = {}
+                interface_ipv4_address_dict_buffer["id"] = "urn:ngsi-ld:Address:" + interface_ipv4_dict_buffer["id"].split(":")[-1]
+                interface_ipv4_address_dict_buffer["type"] = "Address"
+                interface_ipv4_address_dict_buffer["isPartOf"] = {}
+                interface_ipv4_address_dict_buffer["isPartOf"]["type"] = "Relationship"
+                interface_ipv4_address_dict_buffer["isPartOf"]["object"] = interface_ipv4_dict_buffer["id"]
+                ip = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ip")
+                if ip is not None:
+                    element_text = ip.text
+                    interface_ipv4_address_dict_buffer["ip"] = {}
+                    interface_ipv4_address_dict_buffer["ip"]["type"] = "Property"
+                    interface_ipv4_address_dict_buffer["ip"]["value"] = element_text
+                origin = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}origin")
+                if origin is not None:
+                    element_text = origin.text
+                    interface_ipv4_address_dict_buffer["origin"] = {}
+                    interface_ipv4_address_dict_buffer["origin"]["type"] = "Property"
+                    interface_ipv4_address_dict_buffer["origin"]["value"] = element_text
+                interface_ipv4_address_dict_buffers.append(interface_ipv4_address_dict_buffer)
+            for neighbor in ipv4.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}neighbor"):
+                interface_ipv4_neighbor_dict_buffer = {}
+                interface_ipv4_neighbor_dict_buffer["id"] = "urn:ngsi-ld:Neighbor:" + interface_ipv4_dict_buffer["id"].split(":")[-1]
+                interface_ipv4_neighbor_dict_buffer["type"] = "Neighbor"
+                interface_ipv4_neighbor_dict_buffer["isPartOf"] = {}
+                interface_ipv4_neighbor_dict_buffer["isPartOf"]["type"] = "Relationship"
+                interface_ipv4_neighbor_dict_buffer["isPartOf"]["object"] = interface_ipv4_dict_buffer["id"]
+                ip = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ip")
+                if ip is not None:
+                    element_text = ip.text
+                    interface_ipv4_neighbor_dict_buffer["ip"] = {}
+                    interface_ipv4_neighbor_dict_buffer["ip"]["type"] = "Property"
+                    interface_ipv4_neighbor_dict_buffer["ip"]["value"] = element_text
+                linkLayerAddress = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}link-layer-address")
+                if linkLayerAddress is not None:
+                    element_text = linkLayerAddress.text
+                    interface_ipv4_neighbor_dict_buffer["linkLayerAddress"] = {}
+                    interface_ipv4_neighbor_dict_buffer["linkLayerAddress"]["type"] = "Property"
+                    interface_ipv4_neighbor_dict_buffer["linkLayerAddress"]["value"] = element_text
+                origin = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}origin")
+                if origin is not None:
+                    element_text = origin.text
+                    interface_ipv4_neighbor_dict_buffer["origin"] = {}
+                    interface_ipv4_neighbor_dict_buffer["origin"]["type"] = "Property"
+                    interface_ipv4_neighbor_dict_buffer["origin"]["value"] = element_text
+                interface_ipv4_neighbor_dict_buffers.append(interface_ipv4_neighbor_dict_buffer)
+            interface_ipv4_dict_buffers.append(interface_ipv4_dict_buffer)
+        for ipv6 in interface.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ipv6"):
+            interface_ipv6_dict_buffer = {}
+            interface_ipv6_dict_buffer["id"] = "urn:ngsi-ld:Ipv6:" + interface_dict_buffer["id"].split(":")[-1]
+            interface_ipv6_dict_buffer["type"] = "Ipv6"
+            interface_ipv6_dict_buffer["isPartOf"] = {}
+            interface_ipv6_dict_buffer["isPartOf"]["type"] = "Relationship"
+            interface_ipv6_dict_buffer["isPartOf"]["object"] = interface_dict_buffer["id"]
+            enabled = ipv6.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}enabled")
+            if enabled is not None:
+                element_text = enabled.text
+                interface_ipv6_dict_buffer["enabled"] = {}
+                interface_ipv6_dict_buffer["enabled"]["type"] = "Property"
+                interface_ipv6_dict_buffer["enabled"]["value"] = element_text.capitalize()
+            forwarding = ipv6.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}forwarding")
+            if forwarding is not None:
+                element_text = forwarding.text
+                interface_ipv6_dict_buffer["forwarding"] = {}
+                interface_ipv6_dict_buffer["forwarding"]["type"] = "Property"
+                interface_ipv6_dict_buffer["forwarding"]["value"] = element_text.capitalize()
+            mtu = ipv6.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}mtu")
+            if mtu is not None:
+                element_text = mtu.text
+                interface_ipv6_dict_buffer["mtu"] = {}
+                interface_ipv6_dict_buffer["mtu"]["type"] = "Property"
+                interface_ipv6_dict_buffer["mtu"]["value"] = int(element_text)
+            for address in ipv6.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}address"):
+                interface_ipv6_address_dict_buffer = {}
+                interface_ipv6_address_dict_buffer["id"] = "urn:ngsi-ld:Address:" + interface_ipv6_dict_buffer["id"].split(":")[-1]
+                interface_ipv6_address_dict_buffer["type"] = "Address"
+                interface_ipv6_address_dict_buffer["isPartOf"] = {}
+                interface_ipv6_address_dict_buffer["isPartOf"]["type"] = "Relationship"
+                interface_ipv6_address_dict_buffer["isPartOf"]["object"] = interface_ipv6_dict_buffer["id"]
+                ip = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ip")
+                if ip is not None:
+                    element_text = ip.text
+                    interface_ipv6_address_dict_buffer["ip"] = {}
+                    interface_ipv6_address_dict_buffer["ip"]["type"] = "Property"
+                    interface_ipv6_address_dict_buffer["ip"]["value"] = element_text
+                prefixLength = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}prefix-length")
+                if prefixLength is not None:
+                    element_text = prefixLength.text
+                    interface_ipv6_address_dict_buffer["prefixLength"] = {}
+                    interface_ipv6_address_dict_buffer["prefixLength"]["type"] = "Property"
+                    interface_ipv6_address_dict_buffer["prefixLength"]["value"] = int(element_text)
+                origin = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}origin")
+                if origin is not None:
+                    element_text = origin.text
+                    interface_ipv6_address_dict_buffer["origin"] = {}
+                    interface_ipv6_address_dict_buffer["origin"]["type"] = "Property"
+                    interface_ipv6_address_dict_buffer["origin"]["value"] = element_text
+                status = address.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}status")
+                if status is not None:
+                    element_text = status.text
+                    interface_ipv6_address_dict_buffer["status"] = {}
+                    interface_ipv6_address_dict_buffer["status"]["type"] = "Property"
+                    interface_ipv6_address_dict_buffer["status"]["value"] = element_text
+                interface_ipv6_address_dict_buffers.append(interface_ipv6_address_dict_buffer)
+            for neighbor in ipv6.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}neighbor"):
+                interface_ipv6_neighbor_dict_buffer = {}
+                interface_ipv6_neighbor_dict_buffer["id"] = "urn:ngsi-ld:Neighbor:" + interface_ipv6_dict_buffer["id"].split(":")[-1]
+                interface_ipv6_neighbor_dict_buffer["type"] = "Neighbor"
+                interface_ipv6_neighbor_dict_buffer["isPartOf"] = {}
+                interface_ipv6_neighbor_dict_buffer["isPartOf"]["type"] = "Relationship"
+                interface_ipv6_neighbor_dict_buffer["isPartOf"]["object"] = interface_ipv6_dict_buffer["id"]
+                ip = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}ip")
+                if ip is not None:
+                    element_text = ip.text
+                    interface_ipv6_neighbor_dict_buffer["ip"] = {}
+                    interface_ipv6_neighbor_dict_buffer["ip"]["type"] = "Property"
+                    interface_ipv6_neighbor_dict_buffer["ip"]["value"] = element_text
+                linkLayerAddress = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}link-layer-address")
+                if linkLayerAddress is not None:
+                    element_text = linkLayerAddress.text
+                    interface_ipv6_neighbor_dict_buffer["linkLayerAddress"] = {}
+                    interface_ipv6_neighbor_dict_buffer["linkLayerAddress"]["type"] = "Property"
+                    interface_ipv6_neighbor_dict_buffer["linkLayerAddress"]["value"] = element_text
+                origin = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}origin")
+                if origin is not None:
+                    element_text = origin.text
+                    interface_ipv6_neighbor_dict_buffer["origin"] = {}
+                    interface_ipv6_neighbor_dict_buffer["origin"]["type"] = "Property"
+                    interface_ipv6_neighbor_dict_buffer["origin"]["value"] = element_text
+                isRouter = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}is-router")
+                if isRouter is not None:
+                    element_text = isRouter.text
+                    interface_ipv6_neighbor_dict_buffer["isRouter"] = {}
+                    interface_ipv6_neighbor_dict_buffer["isRouter"]["type"] = "Property"
+                    interface_ipv6_neighbor_dict_buffer["isRouter"]["value"] = element_text
+                state = neighbor.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}state")
+                if state is not None:
+                    element_text = state.text
+                    interface_ipv6_neighbor_dict_buffer["state"] = {}
+                    interface_ipv6_neighbor_dict_buffer["state"]["type"] = "Property"
+                    interface_ipv6_neighbor_dict_buffer["state"]["value"] = element_text
+                interface_ipv6_neighbor_dict_buffers.append(interface_ipv6_neighbor_dict_buffer)
+            dupAddrDetectTransmits = ipv6.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}dup-addr-detect-transmits")
+            if dupAddrDetectTransmits is not None:
+                element_text = dupAddrDetectTransmits.text
+                interface_ipv6_dict_buffer["dupAddrDetectTransmits"] = {}
+                interface_ipv6_dict_buffer["dupAddrDetectTransmits"]["type"] = "Property"
+                interface_ipv6_dict_buffer["dupAddrDetectTransmits"]["value"] = int(element_text)
+            for autoconf in ipv6.findall(".//{urn:ietf:params:xml:ns:yang:ietf-ip}autoconf"):
+                interface_ipv6_autoconf_dict_buffer = {}
+                interface_ipv6_autoconf_dict_buffer["id"] = "urn:ngsi-ld:Autoconf:" + interface_ipv6_dict_buffer["id"].split(":")[-1]
+                interface_ipv6_autoconf_dict_buffer["type"] = "Autoconf"
+                interface_ipv6_autoconf_dict_buffer["isPartOf"] = {}
+                interface_ipv6_autoconf_dict_buffer["isPartOf"]["type"] = "Relationship"
+                interface_ipv6_autoconf_dict_buffer["isPartOf"]["object"] = interface_ipv6_dict_buffer["id"]
+                createGlobalAddresses = autoconf.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}create-global-addresses")
+                if createGlobalAddresses is not None:
+                    element_text = createGlobalAddresses.text
+                    interface_ipv6_autoconf_dict_buffer["createGlobalAddresses"] = {}
+                    interface_ipv6_autoconf_dict_buffer["createGlobalAddresses"]["type"] = "Property"
+                    interface_ipv6_autoconf_dict_buffer["createGlobalAddresses"]["value"] = element_text.capitalize()
+                createTemporaryAddresses = autoconf.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}create-temporary-addresses")
+                if createTemporaryAddresses is not None:
+                    element_text = createTemporaryAddresses.text
+                    interface_ipv6_autoconf_dict_buffer["createTemporaryAddresses"] = {}
+                    interface_ipv6_autoconf_dict_buffer["createTemporaryAddresses"]["type"] = "Property"
+                    interface_ipv6_autoconf_dict_buffer["createTemporaryAddresses"]["value"] = element_text.capitalize()
+                temporaryValidLifetime = autoconf.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}temporary-valid-lifetime")
+                if temporaryValidLifetime is not None:
+                    element_text = temporaryValidLifetime.text
+                    interface_ipv6_autoconf_dict_buffer["temporaryValidLifetime"] = {}
+                    interface_ipv6_autoconf_dict_buffer["temporaryValidLifetime"]["type"] = "Property"
+                    interface_ipv6_autoconf_dict_buffer["temporaryValidLifetime"]["value"] = int(element_text)
+                temporaryPreferredLifetime = autoconf.find(".//{urn:ietf:params:xml:ns:yang:ietf-ip}temporary-preferred-lifetime")
+                if temporaryPreferredLifetime is not None:
+                    element_text = temporaryPreferredLifetime.text
+                    interface_ipv6_autoconf_dict_buffer["temporaryPreferredLifetime"] = {}
+                    interface_ipv6_autoconf_dict_buffer["temporaryPreferredLifetime"]["type"] = "Property"
+                    interface_ipv6_autoconf_dict_buffer["temporaryPreferredLifetime"]["value"] = int(element_text)
+                interface_ipv6_autoconf_dict_buffers.append(interface_ipv6_autoconf_dict_buffer)
+            interface_ipv6_dict_buffers.append(interface_ipv6_dict_buffer)
+    interface_dict_buffers.append(interface_dict_buffer)
 
     stop_time = time.perf_counter_ns()
 
