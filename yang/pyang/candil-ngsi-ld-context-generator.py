ee6adddd5431b232290ffc7b07d9b68e6937f8e8
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2023-07-18 11:26:27 +0200

    Some formatting updates to Pyang plugins

--- yang/pyang/candil-ngsi-ld-context-generator.py
+++ yang/pyang/candil-ngsi-ld-context-generator.py
@@ -1,4 +1,4 @@
-"""
+'''
 pyang plugin -- CANDIL NGSI-LD Context Generator.
 
 Generates the NGSI-LD context files associated with a YANG module file following the defined guidelines and conventions.
@@ -7,7 +7,7 @@ The results are written to individual .jsonld files: one for every NGSI-LD Entit
 Version: 0.2.7.
 
 Author: Networking and Virtualization Research Group (GIROS DIT-UPM) -- https://dit.upm.es/~giros
-"""
+'''
 
 import optparse
 import sys
@@ -33,9 +33,9 @@ class CandilNgsiLdContextGeneratorPlugin(plugin.PyangPlugin):
     
     def add_opts(self, optparser):
         optlist = [
-            optparse.make_option("--ngsi_ld_context_generator_help", dest="print_help", action="store_true", help="Prints help and usage")
+            optparse.make_option('--candil-ngsi-ld-ctxgen-help', dest='print_help', action='store_true', help='Prints help and usage')
         ]
-        g = optparser.add_option_group("Execution options")
+        g = optparser.add_option_group('Execution options')
         g.add_options(optlist)
 
     def setup_ctx(self, ctx):
@@ -50,10 +50,10 @@ class CandilNgsiLdContextGeneratorPlugin(plugin.PyangPlugin):
         generate_ngsi_ld_context(ctx, modules, fd)
 
 def print_help():
-    """
+    '''
     Prints plugin's help and usage information.
-    """
-    print("""
+    '''
+    print('''
 Pyang plugin - CANDIL NGSI-LD Context Generator (candil-ngsi-ld-context-generator).
 This plugin generates the NGSI-LD Context files associated with one or several YANG modules
 (for augments) following the defined translation guidelines and conventions.
@@ -62,27 +62,27 @@ has its independent context file.
 
 Usage:
 pyang -f candil-ngsi-ld-context-generator <base_module.yang> [augmenting_module_1.yang] [augmenting_module_2.yang] ...
-    """)
+    ''')
           
 def generate_ngsi_ld_context(ctx, modules, fd):
-    """
+    '''
     Processes a YANG module and generates the corresponding NGSI-LD context files in JSON-LD format.
-    """
+    '''
 
     # Use PDB to debug the code with pdb.set_trace().
 
     # CONSTANTS:
 
-    NGSI_LD_CORE_CONTEXT_URI = "https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context-v1.6.jsonld/"
-    IS_PART_OF_URI = "https://smartdatamodels.org/isPartOf"
+    NGSI_LD_CORE_CONTEXT_URI = 'https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context-v1.6.jsonld/'
+    IS_PART_OF_URI = 'https://smartdatamodels.org/isPartOf'
 
     # AUXILIARY FUNCTIONS: 
 
     def to_camel_case(keyword: str, element_name: str) -> str:
-        """
+        '''
         Auxiliary function.
         Returns the CamelCase representation of element_name according to the YANG to NGSI-LD translation conventions.
-        """
+        '''
         if (keyword is None) or (element_name is None):
             return element_name
         else:
@@ -91,26 +91,26 @@ def generate_ngsi_ld_context(ctx, modules, fd):
             if (keyword == 'container') or (keyword == 'list'):
                 return element_name.capitalize()
             if (keyword == 'leaf') or (keyword == 'leaf-list'):
-                return re.sub(r"(-)(\w)", lambda m: m.group(2).upper(), element_name)
+                return re.sub(r'(-)(\w)', lambda m: m.group(2).upper(), element_name)
     
     def is_enclosing_container(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element is an "enclosing container":
         - It is a container AND
         - It only has one child AND
         - This child is a list.
-        """
+        '''
         result = False
         if (element.keyword == 'container') and (len(element.i_children) == 1) and (element.i_children[0].keyword == 'list'):
             result = True
         return result
     
     def is_deprecated(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element is deprecated.
-        """
+        '''
         result = False
         status = element.search_one('status')
         if (status is not None) and (status.arg == 'deprecated'):
@@ -118,40 +118,40 @@ def generate_ngsi_ld_context(ctx, modules, fd):
         return result
 
     def is_entity(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for an Entity.
-        """
+        '''
         result = False
         if (element.keyword in ['container', 'list']):
             result = True
         return result
     
     def is_property(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for a Property.
-        """
+        '''
         result = False
         if (element.keyword in ['leaf-list', 'leaf']) and ('ref' not in str(element.search_one('type'))):
             result = True
         return result
     
     def is_relationship(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for a Relationship.
-        """
+        '''
         result = False
         if (element.keyword in ['leaf-list', 'leaf']) and ('ref' in str(element.search_one('type'))):
             result = True
         return result
                 
     def generate_context(element, module_name, module_urn, xpath, ngsi_ld_context):
-        """
+        '''
         Auxiliary function.
         Recursively generates the NGSI-LD context(s) given a YANG data node (element) and the X-Path.
-        """
+        '''
         if element.i_module.i_modulename == module.i_modulename:
             name = str(element.arg)
         else:
@@ -161,7 +161,7 @@ def generate_ngsi_ld_context(ctx, modules, fd):
             if (subelements is not None):
                 for subelement in subelements:
                     if (subelement is not None) and (subelement.keyword in statements.data_definition_keywords):
-                        generate_context(subelement, module_name, module_urn, xpath + name + "/", None)
+                        generate_context(subelement, module_name, module_urn, xpath + name + '/', None)
         elif (is_entity(element) == True) and (is_deprecated(element) == False):
                 json_ld = {}
                 json_ld["@context"] = []
@@ -177,11 +177,11 @@ def generate_ngsi_ld_context(ctx, modules, fd):
                 json_ld["@context"].append(ngsi_ld_context)
                 json_ld["@context"].append(NGSI_LD_CORE_CONTEXT_URI)
                 # Help: https://stackoverflow.com/questions/12517451/automatically-creating-directories-with-file-output
-                filename = "ngsi-ld-context/" + xpath.replace("/", "_").replace(":", "_") + str(element.arg) + ".jsonld"
+                filename = 'ngsi-ld-context/' + xpath.replace('/', '_').replace(':', '_') + str(element.arg) + '.jsonld'
                 os.makedirs(os.path.dirname(filename), exist_ok=True)
-                file = open(filename, "w")
+                file = open(filename, 'w')
                 file.write(json.dumps(json_ld, indent=4) + '\n')
-                fd.write("NGSI-LD Context written to " + file.name + "\n")
+                fd.write('NGSI-LD Context written to ' + file.name + '\n')
         elif (is_property(element) == True) and (is_deprecated(element) == False):
             ngsi_ld_context[to_camel_case(str(element.keyword), str(element.arg))] = xpath + name
         elif (is_relationship(element) == True) and (is_deprecated(element) == False):
@@ -191,7 +191,7 @@ def generate_ngsi_ld_context(ctx, modules, fd):
     for module in modules:
         module_name = str(module.arg)
         module_urn = str(module.search_one('namespace').arg)
-        xpath = module_name + ":"
+        xpath = module_name + ':'
         elements = module.i_children
         if (elements is not None):
             for element in elements:
--- yang/pyang/candil-xml-parser-generator.py
+++ yang/pyang/candil-xml-parser-generator.py
@@ -1,4 +1,4 @@
-"""
+'''
 pyang plugin -- CANDIL XML Parser Generator.
 
 Given one or several YANG modules, it dynamically generates the code of an XML parser
@@ -8,7 +8,7 @@ instances of Pydantic classes from the NGSI-LD-backed OpenAPI generation.
 Version: 0.0.8.
 
 Author: Networking and Virtualization Research Group (GIROS DIT-UPM) -- https://dit.upm.es/~giros
-"""
+'''
 
 import optparse
 import sys
@@ -34,9 +34,9 @@ class CandilXmlParserGeneratorPlugin(plugin.PyangPlugin):
     
     def add_opts(self, optparser):
         optlist = [
-            optparse.make_option("--xml_parser_generator_help", dest="print_help", action="store_true", help="Prints help and usage")
+            optparse.make_option('--candil-xmlpgen-help', dest='print_help', action='store_true', help='Prints help and usage')
         ]
-        g = optparser.add_option_group("Execution options")
+        g = optparser.add_option_group('Execution options')
         g.add_options(optlist)
 
     def setup_ctx(self, ctx):
@@ -51,10 +51,10 @@ class CandilXmlParserGeneratorPlugin(plugin.PyangPlugin):
         generate_python_xml_parser_code(ctx, modules, fd)
 
 def print_help():
-    """
+    '''
     Prints plugin's help information.
-    """
-    print("""
+    '''
+    print('''
 Pyang plugin - CANDIL XML Parser Generator (candil-xml-parser-generator).
 Given one or several YANG modules, this plugin generates the Python code of an XML parser
 that is able to read data modeled by these YANG modules and is also able to generate
@@ -63,12 +63,12 @@ according to the OpenAPI generation.
 
 Usage:
 pyang -f candil-xml-parser-generator <base_module.yang> [augmenting_module_1.yang] [augmenting_module_2.yang] ...
-    """)
+    ''')
           
 def generate_python_xml_parser_code(ctx, modules, fd):
-    """
+    '''
     Processes YANG modules and generates the corresponding Python XML parser code for data modeled by these YANG modules.
-    """
+    '''
 
     # Use PDB to debug the code with pdb.set_trace().
 
@@ -77,141 +77,141 @@ def generate_python_xml_parser_code(ctx, modules, fd):
     # NOTE: from ietf-yang-types@2023-01-23.yang.
     # If there are several conversion steps, the value is always the final type.
     IETF_YANG_TYPES_TO_BASE_YANG_TYPES = {
-        "yang:counter32": "uint32",
-        "yang:zero-based-counter32": "uint32",
-        "yang:counter64": "uint64",
-        "yang:zero-based-counter64": "uint64",
-        "yang:gauge32": "uint32",
-        "yang:gauge64": "uint64",
-        "yang:object-identifier": "string",
-        "yang:object-identifier-128": "string",
-        "yang:date-and-time": "string",
-        "yang:date-with-zone-offset": "string",
-        "yang:date-no-zone": "string",
-        "yang:time-with-zone-offset": "string",
-        "yang:time-no-zone": "string",
-        "yang:hours32": "int32",
-        "yang:minutes32": "int32",
-        "yang:seconds32": "int32",
-        "yang:centiseconds32": "int32",
-        "yang:miliseconds32": "int32",
-        "yang:microseconds32": "int32",
-        "yang:microseconds64": "int64",
-        "yang:nanoseconds32": "int32",
-        "yang:nanoseconds64": "int64",
-        "yang:timeticks": "uint32",
-        "yang:timestamp": "uint32",
-        "yang:phys-address": "string",
-        "yang:mac-address": "string",
-        "yang:xpath1.0": "string",
-        "yang:hex-string": "string",
-        "yang:uuid": "string",
-        "yang:dotted-quad": "string",
-        "yang:language-tag": "string",
-        "yang:yang-identifier": "string"
+        'yang:counter32': 'uint32',
+        'yang:zero-based-counter32': 'uint32',
+        'yang:counter64': 'uint64',
+        'yang:zero-based-counter64': 'uint64',
+        'yang:gauge32': 'uint32',
+        'yang:gauge64': 'uint64',
+        'yang:object-identifier': 'string',
+        'yang:object-identifier-128': 'string',
+        'yang:date-and-time': 'string',
+        'yang:date-with-zone-offset': 'string',
+        'yang:date-no-zone': 'string',
+        'yang:time-with-zone-offset': 'string',
+        'yang:time-no-zone': 'string',
+        'yang:hours32': 'int32',
+        'yang:minutes32': 'int32',
+        'yang:seconds32': 'int32',
+        'yang:centiseconds32': 'int32',
+        'yang:miliseconds32': 'int32',
+        'yang:microseconds32': 'int32',
+        'yang:microseconds64': 'int64',
+        'yang:nanoseconds32': 'int32',
+        'yang:nanoseconds64': 'int64',
+        'yang:timeticks': 'uint32',
+        'yang:timestamp': 'uint32',
+        'yang:phys-address': 'string',
+        'yang:mac-address': 'string',
+        'yang:xpath1.0': 'string',
+        'yang:hex-string': 'string',
+        'yang:uuid': 'string',
+        'yang:dotted-quad': 'string',
+        'yang:language-tag': 'string',
+        'yang:yang-identifier': 'string'
     }
 
     # NOTE: from ietf-inet-types@2021-02-22.yang.
     IETF_INET_TYPES_TO_BASE_YANG_TYPES = {
-        "inet:ip-version": "enumeration",
-        "inet:dscp": "uint8",
-        "inet:ipv6-flow-label": "uint32",
-        "inet:port-number": "uint16",
-        "inet:as-number": "uint32",
-        "inet:ip-address": "union",
-        "inet:ipv4-address": "string",
-        "inet:ipv6-address": "string",
-        "inet:ip-address-no-zone": "union",
-        "inet:ipv4-address-no-zone": "string",
-        "inet:ipv6-address-no-zone": "string",
-        "inet:ip-prefix": "union",
-        "inet:ipv4-prefix": "string",
-        "inet:ipv6-prefix": "string",
-        "inet:ip-address-and-prefix": "union",
-        "inet:ipv4-address-and-prefix": "string",
-        "inet:ipv6-address-and-prefix": "string",
-        "inet:domain-name": "string",
-        "inet:host-name": "string",
-        "inet:host": "union",
-        "inet:uri": "string",
-        "inet:email-address": "string"
+        'inet:ip-version': 'enumeration',
+        'inet:dscp': 'uint8',
+        'inet:ipv6-flow-label': 'uint32',
+        'inet:port-number': 'uint16',
+        'inet:as-number': 'uint32',
+        'inet:ip-address': 'union',
+        'inet:ipv4-address': 'string',
+        'inet:ipv6-address': 'string',
+        'inet:ip-address-no-zone': 'union',
+        'inet:ipv4-address-no-zone': 'string',
+        'inet:ipv6-address-no-zone': 'string',
+        'inet:ip-prefix': 'union',
+        'inet:ipv4-prefix': 'string',
+        'inet:ipv6-prefix': 'string',
+        'inet:ip-address-and-prefix': 'union',
+        'inet:ipv4-address-and-prefix': 'string',
+        'inet:ipv6-address-and-prefix': 'string',
+        'inet:domain-name': 'string',
+        'inet:host-name': 'string',
+        'inet:host': 'union',
+        'inet:uri': 'string',
+        'inet:email-address': 'string'
     }
 
     # NOTE: from ietf-ip@2018-02-22.yang.
     IETF_IP_TYPES_TO_BASE_YANG_TYPES = {
-        "ip-address-origin": "enumeration",
-        "neighbor-origin": "enumeration"
+        'ip-address-origin': 'enumeration',
+        'neighbor-origin': 'enumeration'
     }
 
     # NOTE: from netflow-v9.yang and netflow-v9-agg.yang.
     NETFLOW_V9_TYPES_TO_BASE_YANG_TYPES = {
-        "net-v9:prefix-length-ipv4": "uint8",
-        "net-v9:prefix-length-ipv6": "uint8",
-        "net-v9:protocol-type": "enumeration",
-        "net-v9:engine-type": "enumeration",
-        "net-v9:top-label-type": "enumeration",
-        "net-v9:forwarding-status-type": "enumeration",
-        "net-v9:igmp-type": "enumeration",
-        "net-v9:sampling-mode-type": "enumeration",
-        "net-v9:ip-version-type": "enumeration",
-        "net-v9:direction-type": "enumeration",
-        "net-v9:tcp-flags-type": "bits",
-        "per-decimal": "decimal64"
+        'net-v9:prefix-length-ipv4': 'uint8',
+        'net-v9:prefix-length-ipv6': 'uint8',
+        'net-v9:protocol-type': 'enumeration',
+        'net-v9:engine-type': 'enumeration',
+        'net-v9:top-label-type': 'enumeration',
+        'net-v9:forwarding-status-type': 'enumeration',
+        'net-v9:igmp-type': 'enumeration',
+        'net-v9:sampling-mode-type': 'enumeration',
+        'net-v9:ip-version-type': 'enumeration',
+        'net-v9:direction-type': 'enumeration',
+        'net-v9:tcp-flags-type': 'bits',
+        'per-decimal': 'decimal64'
     }
 
     # NOTE: NGSI-LD types are Python "types" (as per this particular implementation).
     BASE_YANG_TYPES_TO_NGSI_LD_TYPES = {
-        "int8": "Integer",
-        "int16": "Integer",
-        "int32": "Integer",
-        "int64": "Integer",
-        "uint8": "Integer",
-        "uint16": "Integer",
-        "uint32": "Integer",
-        "uint64": "Integer",
-        "decimal64": "Integer",
-        "string": "String",
-        "boolean": "Boolean",
-        "enumeration": "String",
-        "bits": "String[]",
-        "binary": "String",
-        "empty": "String",
-        "union": "String"
+        'int8': 'Integer',
+        'int16': 'Integer',
+        'int32': 'Integer',
+        'int64': 'Integer',
+        'uint8': 'Integer',
+        'uint16': 'Integer',
+        'uint32': 'Integer',
+        'uint64': 'Integer',
+        'decimal64': 'Integer',
+        'string': 'String',
+        'boolean': 'Boolean',
+        'enumeration': 'String',
+        'bits': 'String[]',
+        'binary': 'String',
+        'empty': 'String',
+        'union': 'String'
     }
 
     INDENTATION_LEVEL = '    '
 
     BASE_IMPORT_STATEMENTS = [
-        "import sys",
-        "import xml.etree.ElementTree as et",
-        "import logging",
-        "import logging.config",
-        "import yaml",
-        "import os",
-        "import time",
-        "import re",
-        "import subprocess",
-        "import pdb",
-        "import ngsi_ld_client",
-        "from fastapi import FastAPI, Request, status",
-        "from ngsi_ld_client.api_client import ApiClient as NGSILDClient",
-        "from ngsi_ld_client.configuration import Configuration as NGSILDConfiguration",
-        "from ngsi_ld_client.exceptions import ApiException"
+        'import sys',
+        'import xml.etree.ElementTree as et',
+        'import logging',
+        'import logging.config',
+        'import yaml',
+        'import os',
+        'import time',
+        'import re',
+        'import subprocess',
+        'import pdb',
+        'import ngsi_ld_client',
+        'from fastapi import FastAPI, Request, status',
+        'from ngsi_ld_client.api_client import ApiClient as NGSILDClient',
+        'from ngsi_ld_client.configuration import Configuration as NGSILDConfiguration',
+        'from ngsi_ld_client.exceptions import ApiException'
     ]
 
     XML_PARSER_BASE_OPERATIONS = [
-        "xml_file = sys.argv[1]",    
-        "tree = et.parse(xml_file)",
-        "root = tree.getroot()"
+        'xml_file = sys.argv[1]',    
+        'tree = et.parse(xml_file)',
+        'root = tree.getroot()'
     ]
 
     # AUXILIARY FUNCTIONS: 
 
     def to_camel_case(element_keyword: str, element_arg: str) -> str:
-        """
+        '''
         Auxiliary function.
         Returns the CamelCase representation of element_name according to the YANG to NGSI-LD translation conventions.
-        """
+        '''
         if (element_keyword is None) or (element_arg is None):
             return element_arg
         else:
@@ -220,7 +220,7 @@ def generate_python_xml_parser_code(ctx, modules, fd):
             if (element_keyword == 'container') or (element_keyword == 'list'):
                 return element_arg.capitalize()
             if (element_keyword == 'leaf') or (element_keyword == 'leaf-list'):
-                return re.sub(r"(-)(\w)", lambda m: m.group(2).upper(), element_arg)
+                return re.sub(r'(-)(\w)', lambda m: m.group(2).upper(), element_arg)
     
     def yang_to_ngsi_ld_types_conversion(element_type: str) -> str:
         '''
@@ -246,33 +246,33 @@ def generate_python_xml_parser_code(ctx, modules, fd):
         Returns a String with the Python code that implements the correct formatting for the value/text of an element in
         an XML file given the NGSI-LD type of that particular element.
         '''
-        if (ngsi_ld_type == "String"):
+        if (ngsi_ld_type == 'String'):
             return element_text
-        elif (ngsi_ld_type == "String[]"):
+        elif (ngsi_ld_type == 'String[]'):
             return 'list(' + element_text + ')'
-        elif (ngsi_ld_type == "Integer"):
+        elif (ngsi_ld_type == 'Integer'):
             return 'int(' + element_text + ')'
-        elif (ngsi_ld_type == "Boolean"):
+        elif (ngsi_ld_type == 'Boolean'):
             return element_text + '.capitalize()'
     
     def is_enclosing_container(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element is an "enclosing container":
         - It is a container AND
         - It only has one child AND
         - This child is a list.
-        """
+        '''
         result = False
         if (element.keyword == 'container') and (len(element.i_children) == 1) and (element.i_children[0].keyword == 'list'):
             result = True
         return result
 
     def is_deprecated(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element is deprecated.
-        """
+        '''
         result = False
         status = element.search_one('status')
         if (status is not None) and (status.arg == 'deprecated'):
@@ -280,30 +280,30 @@ def generate_python_xml_parser_code(ctx, modules, fd):
         return result
     
     def is_entity(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for an Entity.
-        """
+        '''
         result = False
         if (element.keyword in ['container', 'list']):
             result = True
         return result
     
     def is_property(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for a Property.
-        """
+        '''
         result = False
         if (element.keyword in ['leaf-list', 'leaf']) and ('ref' not in str(element.search_one('type'))):
             result = True
         return result
     
     def is_relationship(element):
-        """
+        '''
         Auxiliary function.
         Checks if an element matches the YANG to NGSI-LD translation convention for a Relationship.
-        """
+        '''
         result = False
         if (element.keyword in ['leaf-list', 'leaf']) and ('ref' in str(element.search_one('type'))):
             result = True
@@ -332,7 +332,7 @@ def generate_python_xml_parser_code(ctx, modules, fd):
                     if (subelement is not None) and (subelement.keyword in statements.data_definition_keywords):
                         generate_entity_import_statements_and_dict_buffers(subelement, None)
         elif (is_entity(element) == True) and (is_deprecated(element) == False):
-            fd.write('\n' + 'from ngsi_ld_models.models.' + str(element.arg) + " import " + camelized_element_arg)
+            fd.write('\n' + 'from ngsi_ld_models.models.' + str(element.arg) + ' import ' + camelized_element_arg)
             fd.write('\n' + current_path + 'dict_buffers = []')
             fd.write('\n')
             subelements = element.i_children
@@ -342,10 +342,10 @@ def generate_python_xml_parser_code(ctx, modules, fd):
                         generate_entity_import_statements_and_dict_buffers(subelement, current_path)
 
     def generate_parser_code(element, parent_element_arg, entity_path: str, depth_level: int):
-        """
+        '''
         Auxiliary function.
         Recursively generates the XML parser code.
-        """
+        '''
         camelized_element_arg = to_camel_case(str(element.keyword), str(element.arg))
         element_namespace = str(element.i_module.search_one('namespace').arg)
         current_path = ''
@@ -382,9 +382,9 @@ def generate_python_xml_parser_code(ctx, modules, fd):
                         generate_parser_code(subelement, element.arg, current_path, depth_level)
             fd.write('\n' + INDENTATION_LEVEL * depth_level + current_path + 'dict_buffers.append(' + current_path + 'dict_buffer)')
         elif (is_property(element) == True) and (is_deprecated(element) == False):
-            fd.write('\n' + INDENTATION_LEVEL * depth_level + camelized_element_arg + " " + "=" + " " + str(parent_element_arg) + ".find(\".//{" + element_namespace + "}" + str(element.arg) + "\")")
+            fd.write('\n' + INDENTATION_LEVEL * depth_level + camelized_element_arg + ' ' + '=' + ' ' + str(parent_element_arg) + '.find(\".//{' + element_namespace + '}' + str(element.arg) + '\")')
             fd.write('\n' + INDENTATION_LEVEL * depth_level + 'if ' + camelized_element_arg + ' is not None:')
-            ngsi_ld_type = yang_to_ngsi_ld_types_conversion(str(element.search_one('type')).replace("type ", ""))
+            ngsi_ld_type = yang_to_ngsi_ld_types_conversion(str(element.search_one('type')).replace('type ', ''))
             text_format = element_text_type_formatting(ngsi_ld_type, 'element_text')
             fd.write('\n' + INDENTATION_LEVEL * depth_level + INDENTATION_LEVEL + 'element_text = ' + camelized_element_arg + '.text')
             if (str(element.arg) == 'name'):
@@ -394,7 +394,7 @@ def generate_python_xml_parser_code(ctx, modules, fd):
             fd.write('\n' + INDENTATION_LEVEL * depth_level + INDENTATION_LEVEL + current_path.replace(str(element.arg) + '_', '') + 'dict_buffer[\"' + camelized_element_arg + '\"][\"value\"] = ' + text_format)
         elif (is_relationship(element) == True) and (is_deprecated(element) == False):
             if (str(element.arg) != 'type'):
-                fd.write('\n' + INDENTATION_LEVEL * depth_level + camelized_element_arg + " " + "=" + " " + str(parent_element_arg) + ".find(\".//{" + element_namespace + "}" + str(element.arg) + "\")")
+                fd.write('\n' + INDENTATION_LEVEL * depth_level + camelized_element_arg + ' ' + '=' + ' ' + str(parent_element_arg) + '.find(\".//{' + element_namespace + '}' + str(element.arg) + '\")')
                 fd.write('\n' + INDENTATION_LEVEL * depth_level + 'if ' + camelized_element_arg + ' is not None:')
                 fd.write('\n' + INDENTATION_LEVEL * depth_level + INDENTATION_LEVEL + 'element_text = ' + camelized_element_arg + '.text')
                 fd.write('\n' + INDENTATION_LEVEL * depth_level + INDENTATION_LEVEL + current_path.replace(str(element.arg) + '_', '') + 'dict_buffer[\"' + camelized_element_arg + '\"] = {}')
@@ -404,13 +404,13 @@ def generate_python_xml_parser_code(ctx, modules, fd):
     # Generate XML parser Python code:
     for import_statement in BASE_IMPORT_STATEMENTS:
         fd.write(import_statement)
-        fd.write("\n")
+        fd.write('\n')
 
-    fd.write("\n")
+    fd.write('\n')
 
     for line in XML_PARSER_BASE_OPERATIONS:
         fd.write(line)
-        fd.write("\n")
+        fd.write('\n')
 
     for module in modules:
         elements = module.i_children
@@ -420,4 +420,4 @@ def generate_python_xml_parser_code(ctx, modules, fd):
                     generate_entity_import_statements_and_dict_buffers(element, None)
                     generate_parser_code(element, None, None, 0)
     
-    fd.write("\n")
+    fd.write('\n')
