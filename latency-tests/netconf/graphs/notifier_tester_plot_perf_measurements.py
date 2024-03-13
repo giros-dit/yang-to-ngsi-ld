be420b15620fde09a8043627261684eb0375b9b1
David Martínez García <94689492+david-martinez-garcia@users.noreply.github.com>
2024-03-13 12:14:18 +0100

    Upload some updates

---
+++ latency-tests/netconf/graphs/notifier_tester_plot_perf_measurements.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['Notifications received', 'Notification evaluation time', 'Mean evaluation time']
+
+df = pd.read_csv('notifier_tester_perf.csv', names = headers)
+print(df.dtypes)
+
+df.set_index('Notifications received').plot()
+
+plt.title('notifier-tester -- Evaluation times per notification received')
+plt.xlabel('notification index')
+plt.ylabel('time (in ms)')
+plt.show()
