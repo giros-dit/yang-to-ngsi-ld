ad583955aabb0cbfa6a93f9aaa0db919730b9564
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-03-20 18:24:32 +0100

    Updating topology-discoverer microservice to allow parametrize the periodic interval for getting the network topology information.

--- testbeds/gnmi/docker/topology-discoverer/discover-topology.sh
+++ testbeds/gnmi/docker/topology-discoverer/discover-topology.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-interval=30
+interval=$2
 
 if [[ "$1" == "ceos-scenario" ]]; then
     while true; do
         java -jar topology-driver-1.0.jar /opt/topology-data/ceos-scenario/clab-telemetry-testbed-ceos-ceos-4hosts/topology-data.json topology-data-compliant-yang.json topology-data-compliant-yang.xml
-        sleep 1
+        sleep 0.5
         poetry run python -u candil_topology_discoverer_ngsi_ld_instantiator.py
         sleep $interval
     done
 elif [[ "$1" == "srlinux-scenario" ]]; then
     while true; do
         java -jar topology-driver-1.0.jar /opt/topology-data/srlinux-scenario/clab-telemetry-testbed-srl-srl-4hosts/topology-data.json topology-data-compliant-yang.json topology-data-compliant-yang.xml
-        sleep 1
+        sleep 0.5
         poetry run python -u candil_topology_discoverer_ngsi_ld_instantiator.py
         sleep $interval
     done
