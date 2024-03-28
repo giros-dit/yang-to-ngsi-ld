9a591f63ff5df33a122fdbeb04349f8c9b0f81cf
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-03-28 12:39:35 +0100

    Fixing minor issues within the gNMI testbed for covering the YANG to NGSI-LD translation process.

--- testbeds/gnmi/docker/gnmic-collector-queries/gnmic-collector-queries-kafka-ceos-scenario.py
+++ testbeds/gnmi/docker/gnmic-collector-queries/gnmic-collector-queries-kafka-ceos-scenario.py
@@ -1,9 +1,10 @@
 import subprocess
 from kafka import KafkaProducer
 import time
+import sys
 
 while True:
-    # Get Query  RPC to get all the information about openconfig-interfaces YANG model from cEOS routers:
+    # Get Query RPC to get all the information about openconfig-interfaces YANG model from cEOS routers:
     query = subprocess.run(["gnmic", "get", "--config", "gnmic-request-ceos1.yaml"], capture_output=True, text=True)
 
     producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
@@ -15,7 +16,7 @@ while True:
     producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
     producer.send('interfaces-state-queries', value=query.stdout.strip().encode('utf-8'))
     producer.flush()
-    time.sleep(60)
+    time.sleep(int(sys.argv[1]))
 
 # Subscribe RPC to get all the counters information about openconfig-interfaces YANG model from cEOS routers:
 # subprocess.run(["gnmic", "subscribe", "--config", "gnmic-subs-ceos-ceos.yaml", "--target", "ceos1"])
--- testbeds/gnmi/docker/gnmic-collector-queries/gnmic-collector-queries-kafka-srl-scenario.py
+++ testbeds/gnmi/docker/gnmic-collector-queries/gnmic-collector-queries-kafka-srl-scenario.py
@@ -1,9 +1,10 @@
 import subprocess
 from kafka import KafkaProducer
 import time
+import sys
 
 while True:
-    # Get Query  RPC to get all the information about openconfig-interfaces YANG model from SRLinux routers:
+    # Get Query RPC to get all the information about openconfig-interfaces YANG model from SRLinux routers:
     query = subprocess.run(["gnmic", "get", "--config", "gnmic-request-srl1.yaml"], capture_output=True, text=True)
 
     producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
@@ -15,7 +16,7 @@ while True:
     producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
     producer.send('interfaces-state-queries', value=query.stdout.strip().encode('utf-8'))
     producer.flush()
-    time.sleep(600)
+    time.sleep(int(sys.argv[1]))
 
 # Subscribe RPC to get all the counters information about openconfig-interfaces YANG model from SRLinux routers:
 # subprocess.run(["gnmic", "subscribe", "--config", "gnmic-subs-srl-srl.yaml", "--target", "srl1"])
