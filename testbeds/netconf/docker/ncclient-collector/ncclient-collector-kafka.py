4d48ba518184fad74f32330a6f3631dbe3b69ec5
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-04-28 21:35:42 +0200

    Updating ncclient collector microservice to measure the time difference between the eventTime of notifications received by YANG-push through NETCONF and the successively subsequent instant.

--- testbeds/netconf/docker/ncclient-collector/ncclient-collector-kafka.py
+++ testbeds/netconf/docker/ncclient-collector/ncclient-collector-kafka.py
@@ -7,6 +7,12 @@ from ncclient.xml_ import to_ele
 
 import xml.etree.ElementTree as et
 
+import time
+import datetime
+from dateutil import parser
+
+import csv
+
 if len(sys.argv) < 4:
     print("Error - Incorrect arguments")
     print('Usage: python ncclient-collector-kafka.py <container_name> <interface_name> <period_in_cs>')
@@ -23,6 +29,12 @@ r = {
     "hostkey_verify": False,
     "device_params": {"name": "csr"}
 }
+
+performance_measurements_file = open("performance_measurements-"+container_name+".csv", "w", newline='')
+csv_writer = csv.writer(performance_measurements_file)
+csv_header = ["evenTime", "start_datetime", "evaluation_time", "mean_evaluation_time",
+              "min_evaluation_time", "max_evaluation_time", "notifications_received"]
+csv_writer.writerow(csv_header)  
 
 print("Hello, this is the ncclient-collector for " + container_name + " interface " + sys.argv[2])
 
@@ -56,13 +68,15 @@ request = session.dispatch(to_ele(rpc))
 producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
 
 print("I have subscribed myself to get periodic YANG-Push notifications for X-Path " + xpath + " of network device " + container_name)
+delta_times = []
 
 while True:
     sub_data = session.take_notification()
-    print("I have received a notification!")
+    print("\nI have received a notification!" + "\n")
     #print(str(sub_data.notification_xml).encode('utf-8'))
     if (sub_data != None):
         notification_xml = str(sub_data.notification_xml)
+        start_datetime = datetime.datetime.now(datetime.timezone.utc)
         root = et.fromstring(notification_xml)
         # A new subelement is added to the NETCONF notification: fromDevice.
         # It is the name of the device that is sending the notification.
@@ -70,8 +84,28 @@ while True:
         from_device = et.SubElement(root, 'fromDevice')
         from_device.text = container_name
         eventTime = root[0].text
+        delta_time = (start_datetime - parser.parse(eventTime)).total_seconds()
+        delta_times.append(delta_time)
+        print("--- PERFORMANCE MEASUREMENTS ---\n")
+        print("EVENT TIME: " + parser.parse(eventTime).strftime("%Y-%m-%dT%H:%M:%S.%fZ") + "\n")           
+        print("NOTIFICATION RECEIVED AT: " + start_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ") + "\n")
+        print("NOTIFICATIONS RECEIVED SO FAR: " + str(len(delta_times)) + "\n")
+        mean_evaluation_time = sum(delta_times)/len(delta_times)
+        min_evaluation_time = min(delta_times)
+        max_evaluation_time = max(delta_times)
+        print(f"ITERATION EXECUTION TIME: {delta_time * 1e3} ms\n")
+        print(f"MEAN EVALUATION TIME: {mean_evaluation_time * 1e3} ms\n")
+        print(f"MIN EVALUATION TIME: {min_evaluation_time * 1e3} ms\n")
+        print(f"MAX EVALUATION TIME VALUE: {max_evaluation_time * 1e3} ms\n")
+        print("--- PERFORMANCE MEASUREMENTS ---\n")
+        csv_data = [parser.parse(eventTime).strftime("%Y-%m-%dT%H:%M:%S.%fZ"), start_datetime.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+                    str(delta_time * 1e3) + " ms", str(mean_evaluation_time * 1e3) + " ms",
+                    str(min_evaluation_time * 1e3) + " ms", str(max_evaluation_time * 1e3) + " ms",
+                    str(len(delta_times))]
+        csv_writer.writerow(csv_data)
+        performance_measurements_file.flush()
         notification_xml = et.tostring(root, encoding='unicode')
         producer.send('interfaces-state-subscriptions', value=notification_xml.encode('utf-8'))
         print("I have sent it to a Kafka topic named interfaces-state-subscriptions")
         print("The eventTime element of the notification is: " + eventTime)
-    producer.flush()
+        producer.flush()
