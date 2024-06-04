101ffc6aa79560db8c8fc6361d3983a2ba613bfa
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-06-04 16:17:50 +0200

    Fixing and updating minor things within hte graphs of the testbed performance experiments.

--- testbeds/performance-experiments/experiment2/performance_measurements_translation_and_materialization-combined-orion.py
+++ testbeds/performance-experiments/experiment2/performance_measurements_translation_and_materialization-combined-orion.py
@@ -4,27 +4,27 @@ from matplotlib.patches import Patch
 
 # Create custom legend handles
 legend_handles = [
-    Patch(facecolor='white', edgecolor='cyan', label='NETCONF'),
-    Patch(facecolor='white', edgecolor='orange', label='gNMI')
+    Patch(facecolor='grey', edgecolor='yellow', label='NETCONF'),
+    Patch(facecolor='blue', edgecolor='red', label='gNMI')
 ]
 
-raw_gnmi_df1 = pd.read_csv('gnmi_performance_measurements_translation_12ifaces.csv')
+raw_gnmi_df1 = pd.read_csv('gnmi_performance_measurements_translation_and_materialization_12ifaces-orion.csv')
 sum_by_12_rows = raw_gnmi_df1.groupby(raw_gnmi_df1.index // 12)['iteration_execution_time'].sum()
 gnmi_df1 = pd.DataFrame({'iteration_execution_time': sum_by_12_rows})
 
-raw_gnmi_df2 = pd.read_csv('gnmi_performance_measurements_translation_24ifaces.csv')
+raw_gnmi_df2 = pd.read_csv('gnmi_performance_measurements_translation_and_materialization_24ifaces-orion.csv')
 sum_by_24_rows = raw_gnmi_df2.groupby(raw_gnmi_df2.index // 24)['iteration_execution_time'].sum()
 gnmi_df2 = pd.DataFrame({'iteration_execution_time': sum_by_24_rows})
 
-raw_gnmi_df3 = pd.read_csv('gnmi_performance_measurements_translation_36ifaces.csv')
+raw_gnmi_df3 = pd.read_csv('gnmi_performance_measurements_translation_and_materialization_36ifaces-orion.csv')
 sum_by_36_rows = raw_gnmi_df3.groupby(raw_gnmi_df3.index // 36)['iteration_execution_time'].sum()
 gnmi_df3 = pd.DataFrame({'iteration_execution_time': sum_by_36_rows})
 
-raw_gnmi_df4 = pd.read_csv('gnmi_performance_measurements_translation_48ifaces.csv')
+raw_gnmi_df4 = pd.read_csv('gnmi_performance_measurements_translation_and_materialization_48ifaces-orion.csv')
 sum_by_48_rows = raw_gnmi_df4.groupby(raw_gnmi_df4.index // 48)['iteration_execution_time'].sum()
 gnmi_df4 = pd.DataFrame({'iteration_execution_time': sum_by_48_rows})
 
-print("gNMI performance measurements for YANG to NGSI-LD translation: \n")
+print("gNMI performance measurements for YANG to NGSI-LD translation and materialization: \n")
 
 gnmi_raw_mean_1 = raw_gnmi_df1['iteration_execution_time'].mean()
 gnmi_raw_standard_deviation_1 = gnmi_df1['iteration_execution_time'].std()
@@ -68,21 +68,21 @@ print("\n")
 
 fig, ax = plt.subplots()
 
-gnmi_plot1 = gnmi_df1.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "orange", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[1])
+gnmi_plot1 = gnmi_df1.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "red", linewidth = 1.5), boxprops = dict(facecolor = "blue"), ax=ax, positions=[1])
 
-gnmi_plot2 = gnmi_df2.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "orange", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[2])
+gnmi_plot2 = gnmi_df2.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "red", linewidth = 1.5), boxprops = dict(facecolor = "blue"), ax=ax, positions=[2])
 
-gnmi_plot3 = gnmi_df3.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "orange", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[3])
+gnmi_plot3 = gnmi_df3.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "red", linewidth = 1.5), boxprops = dict(facecolor = "blue"), ax=ax, positions=[3])
 
-gnmi_plot4 = gnmi_df4.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "orange", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[4])
+gnmi_plot4 = gnmi_df4.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "red", linewidth = 1.5), boxprops = dict(facecolor = "blue"), ax=ax, positions=[4])
 
 
-netconf_df1 = pd.read_csv('netconf_performance_measurements_translation_12ifaces.csv')
-netconf_df2 = pd.read_csv('netconf_performance_measurements_translation_24ifaces.csv')
-netconf_df3 = pd.read_csv('netconf_performance_measurements_translation_36ifaces.csv')
-netconf_df4 = pd.read_csv('netconf_performance_measurements_translation_48ifaces.csv')
+netconf_df1 = pd.read_csv('netconf_performance_measurements_translation_and_materialization_12ifaces-orion.csv')
+netconf_df2 = pd.read_csv('netconf_performance_measurements_translation_and_materialization_24ifaces-orion.csv')
+netconf_df3 = pd.read_csv('netconf_performance_measurements_translation_and_materialization_36ifaces-orion.csv')
+netconf_df4 = pd.read_csv('netconf_performance_measurements_translation_and_materialization_48ifaces-orion.csv')
 
-print("NETCONF performance measurements for YANG to NGSI-LD translation: \n")
+print("NETCONF performance measurements for YANG to NGSI-LD translation and materialization: \n")
 
 netconf_mean_1 = netconf_df1['iteration_execution_time'].mean()
 netconf_standard_deviation_1 = netconf_df1['iteration_execution_time'].std()
@@ -108,19 +108,19 @@ print("48 Interfaces - Mean:", netconf_mean_4)
 print("48 Interfaces - Standard deviation:", netconf_standard_deviation_4)
 print("\n")
 
-netconf_plot1 = netconf_df1.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "cyan", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[1])
+netconf_plot1 = netconf_df1.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "yellow", linewidth = 1.5), boxprops = dict(facecolor = "grey"), ax=ax, positions=[1])
 
-netconf_plot2 = netconf_df2.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "cyan", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[2])
+netconf_plot2 = netconf_df2.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "yellow", linewidth = 1.5), boxprops = dict(facecolor = "grey"), ax=ax, positions=[2])
 
-netconf_plot3 = netconf_df3.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "cyan", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[3])
+netconf_plot3 = netconf_df3.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "yellow", linewidth = 1.5), boxprops = dict(facecolor = "grey"), ax=ax, positions=[3])
 
-netconf_plot4 = netconf_df4.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "cyan", linewidth = 1.5), boxprops = dict(facecolor = "white"), ax=ax, positions=[4])
+netconf_plot4 = netconf_df4.boxplot(column = ['iteration_execution_time'], vert=True, patch_artist=True, showfliers = False, medianprops = dict(color = "yellow", linewidth = 1.5), boxprops = dict(facecolor = "grey"), ax=ax, positions=[4])
 
 plt.legend(handles=legend_handles)
 plt.xlabel('Number of network interfaces')
 plt.ylabel('Latency (milliseconds)')
-plt.title('YANG to NGSI-LD translation \n performance for NETCONF and gNMI')
+plt.title('YANG to NGSI-LD translation and materialization \n performance for NETCONF and gNMI')
 plt.xticks([1, 2, 3, 4], ['12', '24', '36', '48'])
 plt.suptitle('')
-plt.savefig("performance_measurements_translation-combined.png", format="png", dpi=1500)
+plt.savefig("performance_measurements_translation_and_materialization-combined-orion.png", format="png", dpi=1500)
 plt.show()
