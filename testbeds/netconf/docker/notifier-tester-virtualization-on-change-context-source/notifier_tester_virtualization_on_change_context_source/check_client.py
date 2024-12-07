4184a3e3090822bebda1791883346124dc7e4e41
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-12-07 21:15:37 +0100

    Generating a new version of the notifier-tester microservice for testing on-change susbcriptions supported by the NGSI-LD Context Source within NETCONF testbed using NDT automation mechanisms based on a data virtualization approach.

---
+++ testbeds/netconf/docker/notifier-tester-virtualization-on-change-context-source/notifier_tester_virtualization_on_change_context_source/check_client.py
@@ -0,0 +1,56 @@
+import logging
+import time
+
+from notifier_tester_virtualization_on_change_context_source.check_api import NGSILDHealthInfoAPI
+
+logger = logging.getLogger(__name__)
+
+
+class NGSILDHealthInfoClient(object):
+    """
+    Class encapsulating the main operations with NGSI-LD.
+    """
+
+    def __init__(self, url: str = "http://scorpio:9090",
+                 headers: dict = {"Accept": "application/json"},
+                 context: str = "http://context-catalog:8080/context.jsonld",
+                 debug: bool = False):
+        # Init NGSI-LD REST API Client
+        self.api = NGSILDHealthInfoAPI(url, headers=headers,
+                             context=context, debug=debug)
+
+    def check_scorpio_status(self):
+        """
+        Infinite loop that checks every 1 second
+        until Scorpio REST API becomes available.
+        """
+        logger.info("Checking Scorpio REST API status ...")
+        while True:
+            if self.api.checkScorpioHealth().ok:
+                logger.info(
+                    "Successfully connected to Scorpio REST API!")
+                logger.info(self.api.checkScorpioHealth().json())
+                break
+            else:
+                logger.info("Could not connect to Scorpio REST API. "
+                               "Retrying in 1 second ...")
+                time.sleep(1)
+                continue
+
+    def check_scorpio_info(self):
+        """
+        Infinite loop that checks every 1 second
+        until Scorpio REST API becomes available.
+        """
+        logger.info("Checking Scorpio build information ...")
+        while True:
+            if self.api.checkScorpioInfo().ok:
+                logger.info(
+                    "Successfully connected to Scorpio REST API!")
+                logger.info(self.api.checkScorpioInfo().json())
+                break
+            else:
+                logger.info("Could not connect to Scorpio REST API. "
+                               "Retrying in 1 second ...")
+                time.sleep(1)
+                continue
