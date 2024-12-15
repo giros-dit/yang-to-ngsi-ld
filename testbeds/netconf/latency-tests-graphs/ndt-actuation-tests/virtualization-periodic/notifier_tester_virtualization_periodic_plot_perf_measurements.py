36e5b360524346c10450404f94ebcdef52cee61c
daniel-gonzalez-sanchez <daniel.gonzalez.sanchez@alumnos.upm.es>
2024-12-15 23:58:44 +0100

    Including new graphics to illustrate the real-time latency performance of data materialization and data materialization NDT approaches on NETCONF testbed.

---
+++ testbeds/netconf/latency-tests-graphs/ndt-actuation-tests/virtualization-periodic/notifier_tester_virtualization_periodic_plot_perf_measurements.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+plt.rcParams["figure.figsize"] = [7.50, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
+headers = ['notifications_received', 'evaluation_time', 'mean_evaluation_time']
+
+df = pd.read_csv('performance_measurements_virtualization_periodic.csv')
+
+selected_df = df[headers]
+
+print(selected_df.dtypes)
+print(selected_df)
+
+selected_df.set_index('notifications_received').plot()
+
+plt.title('notifier-tester-virtualization-periodic \n Evaluation times per notification until entity created')
+plt.xlabel('notification index')
+plt.ylabel('time (in ms)')
+plt.savefig("notifier_tester_virtualization_periodic_perf_graph_entity_created.png")
+plt.show()
+
+
+headers = ['notifications_received', 'evaluation_time', 'mean_evaluation_time']
+
+df = pd.read_csv('notification_performance_measurements_virtualization_periodic.csv')
+
+selected_df = df[headers]
+
+print(selected_df.dtypes)
+print(selected_df)
+
+selected_df.set_index('notifications_received').plot()
+
+plt.title('notifier-tester-virtualization-periodic \n Evaluation times per notification until notification received')
+plt.xlabel('notification index')
+plt.ylabel('time (in ms)')
+plt.savefig("notifier_tester_virtualization_periodic_perf_graph_notification_received.png")
+plt.show()
